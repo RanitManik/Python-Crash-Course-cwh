bcbe41b3c2733127565456f51e492c3030ce7ad5
RanitManik <ranitmanikofficial@outlook.com>
2023-09-08 15:38:52 +0530

    added

--- assignments/program_22.py
+++ assignments/program_22.py
@@ -1,7 +1,7 @@
 # This is program 22
 i = 0
-for i in range(5):
+for i in range(1, 6):
     if i == 3:
         continue
         # This will skip 4 and continue printing
-    print(i + 1)
+    print(i)
